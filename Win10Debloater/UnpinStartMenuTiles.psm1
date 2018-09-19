a8e554081375e9d0bc30ee9144881e05e1c32f3e
sleevezip <mau10rits@gmail.com>
2018-09-19 19:21:42 +0200

    Update UnpinStartMenuTiles.psm1

--- Win10Debloater/UnpinStartMenuTiles.psm1
+++ Win10Debloater/UnpinStartMenuTiles.psm1
@@ -1,14 +1,106 @@
-Function UnpinStartMenuTiles {
-	Write-Output "Unpinning all Start Menu tiles..."
-	If ([System.Environment]::OSVersion.Version.Build -ge 15063 -And [System.Environment]::OSVersion.Version.Build -le 16299) {
-		Get-ChildItem -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount" -Include "*.group" -Recurse | ForEach-Object {
-			$data = (Get-ItemProperty -Path "$($_.PsPath)\Current" -Name "Data").Data -Join ","
-			$data = $data.Substring(0, $data.IndexOf(",0,202,30") + 9) + ",0,202,80,0,0"
-			Set-ItemProperty -Path "$($_.PsPath)\Current" -Name "Data" -Type Binary -Value $data.Split(",")
-		}
-	} ElseIf ([System.Environment]::OSVersion.Version.Build -eq 17134) {
-		$key = Get-ChildItem -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount" -Recurse | Where-Object { $_ -like "*start.tilegrid`$windows.data.curatedtilecollection.tilecollection\Current" }
-		$data = (Get-ItemProperty -Path $key.PSPath -Name "Data").Data[0..25] + ([byte[]](202,50,0,226,44,1,1,0,0))
-		Set-ItemProperty -Path $key.PSPath -Name "Data" -Type Binary -Value $data
-	}
+#
+# This sets the unpin command to search for.
+#
+$UNPIN_LABEL = "Unpin from Start"
+
+#
+# Gets all of the COM Application objects.
+#
+# Each of these will have a child with `verbs` to `pin/unpin` (i.e. Right-Click GUI actions).
+#
+function Get-ApplicationObjects ()
+{
+    #Magic Number; Doesn't cover all live tiles. TODO: find ids for other tiles.
+    $COM_ID_Applications = '{4234d49b-0245-4df3-b780-3893943456e1}'
+
+    $obj = New-Object -Com Shell.Application
+    return $obj.NameSpace("shell:::$COM_ID_Applications")
 }
+
+#
+# Gets the `Unpin...` Verb for an element of `Get-Application-Objects`
+#
+function Get-UnpinVerb ($item)
+{
+  return $item.Verbs() | ?{$_.Name.replace('&','') -match "$UNPIN_LABEL"}
+}
+
+#
+# Filter `Get-Application-Objects` for elements with a valid `Get-Verb-Unpin`.
+#
+function Get-UnpinnableApps()
+{
+  $apps = Get-ApplicationObjects
+  return $apps.Items() | ?{Get-UnpinVerb($_)}
+}
+
+#
+# Execute the unpin verb for an application node.
+#
+function Use-Unpin($item)
+{
+  $verb = Get-UnpinVerb($item)
+  $verb.DoIt()
+}
+
+#
+# Execute the unpin for all unpinnable apps
+#
+function Use-UnpinAll()
+{
+  Get-UnpinnableApps | %{ Use-Unpin($_) }
+}
+
+#
+# List Unpinnable apps to the console.
+#
+function Show-UnpinnableApps()
+{
+  echo "
+  Applications With '$UNPIN_LABEL':
+  "
+
+  Get-UnpinnableApps | %{'    ' + $_.Name}
+}
+
+#
+# A simple CLI confirm/cancel loop.
+#
+function Use-Guard([string]$prompt)
+{
+  $message =
+  "
+  ${prompt}? [y/n]
+  "
+
+  do
+  {
+    $confirmation = Read-Host $message
+    if ($confirmation -eq 'n') {exit}
+  }
+  while($confirmation -ne "y");
+}
+
+#
+# CLI command to remove all start tiles
+#
+# @option -y Confirm operation without user prompt.
+#
+function Remove-StartTiles
+{
+  param(
+    [switch]$y = $false
+  )
+
+  Show-UnpinnableApps
+
+  if ( -not $y ) {Use-Guard("Clear All")}
+
+  echo '
+  Cleaning:
+  '
+  Use-UnpinAll
+}
+
+Export-ModuleMember -function Show-StartTiles
+Export-ModuleMember -function Remove-StartTiles
