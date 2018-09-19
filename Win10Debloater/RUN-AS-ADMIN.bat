4924acc893696c8b395f7dc89ad85be87302903f
sleevezip <mau10rits@gmail.com>
2018-09-19 19:15:53 +0200

    Update RUN-AS-ADMIN.bat

--- Win10Debloater/RUN-AS-ADMIN.bat
+++ Win10Debloater/RUN-AS-ADMIN.bat
@@ -8,10 +8,7 @@ PowerShell -ExecutionPolicy Bypass ".\DisableTelemetryServices.ps1"
 
 PowerShell -ExecutionPolicy Bypass ".\RemovePins.ps1"
 
-
-#Disable Suggestions in Start
-reg add "HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\ContentDeliveryManager\" /v "SystemPaneSuggestionsEnabled\" /t REG_DWORD /d 0 /f
-REM 1 = This PC, 2 = Quick access
-REM reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "LaunchTo" /t REG_DWORD /d 1 /f
+Reg add /"HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\ContentDeliveryManager\" /v "SystemPaneSuggestionsEnabled\" /t REG_DWORD /d 0 /f
+Reg add /"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "LaunchTo" /t REG_DWORD /d 1 /f
 
 FinishedMessage.vbs
