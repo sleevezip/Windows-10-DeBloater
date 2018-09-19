50dd98120a56e07b2a64dd830c299ff6afb84684
sleevezip <mau10rits@gmail.com>
2018-09-19 18:54:14 +0200

    Create Remove Pins.ps1

---
+++ Win10Debloater/Remove Pins.ps1
@@ -0,0 +1,32 @@
+Pin-App "Mail" -unpin
+Pin-App "Store" -unpin
+Pin-App "Calendar" -unpin
+Pin-App "Microsoft Edge" -unpin
+Pin-App "Photos" -unpin
+Pin-App "Cortana" -unpin
+Pin-App "Weather" -unpin
+Pin-App "Phone Companion" -unpin
+Pin-App "Music" -unpin
+Pin-App "xbox" -unpin
+Pin-App "movies & tv" -unpin
+Pin-App "microsoft solitaire collection" -unpin
+Pin-App "money" -unpin
+Pin-App "get office" -unpin
+Pin-App "onenote" -unpin
+Pin-App "news" -unpin
+Pin-App "Mail" -pin
+Pin-App "Store" -pin
+Pin-App "Calendar" -pin
+Pin-App "Microsoft Edge" -pin
+Pin-App "Photos" -pin
+Pin-App "Cortana" -pin
+Pin-App "Weather" -pin
+Pin-App "Phone Companion" -pin
+Pin-App "Music" -pin
+Pin-App "xbox" -pin
+Pin-App "movies & tv" -pin
+Pin-App "microsoft solitaire collection" -pin
+Pin-App "money" -pin
+Pin-App "get office" -pin
+Pin-App "onenote" -pin
+Pin-App "news" -pin
