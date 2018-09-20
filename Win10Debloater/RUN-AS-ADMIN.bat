e31aa6ab9499cea65b2bd1c65a657598cd28c293
sleevezip <mau10rits@gmail.com>
2018-09-20 12:00:43 +0200

    Update RUN-AS-ADMIN.bat

--- Win10Debloater/RUN-AS-ADMIN.bat
+++ Win10Debloater/RUN-AS-ADMIN.bat
@@ -2,9 +2,11 @@ pushd %~dp0
 
 PowerShell -ExecutionPolicy Bypass ".\RemovePins.ps1"
 
-registrychanges.reg
-
 PowerShell -ExecutionPolicy Bypass ".\RemoveBuiltInPrograms.ps1"
+
+PowerShell -ExecutionPolicy Bypass ".\RemoveBuiltInGames.ps1"
+
+registrychanges.reg
 
 PowerShell -ExecutionPolicy Bypass ".\DisableTelemetryServices.ps1"
 
