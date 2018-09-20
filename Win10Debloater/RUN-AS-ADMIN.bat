c9d8e190c61db1c235bfc04d853d3dd3bf9657ce
sleevezip <mau10rits@gmail.com>
2018-09-20 10:45:47 +0200

    Update RUN-AS-ADMIN.bat

--- Win10Debloater/RUN-AS-ADMIN.bat
+++ Win10Debloater/RUN-AS-ADMIN.bat
@@ -1,12 +1,12 @@
 pushd %~dp0
 
+PowerShell -ExecutionPolicy Bypass ".\RemovePins.ps1"
+
 registrychanges.reg
 
 PowerShell -ExecutionPolicy Bypass ".\RemoveBuiltInPrograms.ps1"
 
 PowerShell -ExecutionPolicy Bypass ".\DisableTelemetryServices.ps1"
-
-PowerShell -ExecutionPolicy Bypass ".\RemovePins.ps1"
 
 Reg add /"HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\ContentDeliveryManager\" /v "SystemPaneSuggestionsEnabled\" /t REG_DWORD /d 0 /f
 Reg add /"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "LaunchTo" /t REG_DWORD /d 1 /f
